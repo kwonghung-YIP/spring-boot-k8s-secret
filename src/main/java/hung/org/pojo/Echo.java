4d6d8fae4f012f78e150fe747aaefd47aa70c89c
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2019-07-22 02:40:01 +0800

    Create Echo.java

---
+++ src/main/java/hung/org/pojo/Echo.java
@@ -0,0 +1,12 @@
+package hung.org.pojo;
+
+public class Echo {
+		
+		public String sessionId;
+		public InetAddress ip;
+		public String hostname;
+		public String dbUser;
+		public String dbVersion;
+		public String database;
+    
+	}
