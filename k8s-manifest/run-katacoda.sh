50cb446b8c83d56316bfa55de0773bcb77bff573
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2019-08-09 16:14:15 +0800

    Update run-katacoda.sh

--- k8s-manifest/run-katacoda.sh
+++ k8s-manifest/run-katacoda.sh
@@ -5,3 +5,9 @@ kubectl create configmap redis-config --from-file redis.conf
 kubectl apply -f redis-service.yaml
 kubectl apply -f mysql-service.yaml
 kubectl apply -f springboot-basic-auth.yaml
+
+sleep 10
+kubectl get all
+
+CLUSERIP=`kubectl get service springboot-svc -o=jsonpath='{.spec.clusterIP}'`
+curl -v -user john:abcd1234 $CLUSTERIP:8080
