bbab6ab5a6bed81b44a8a33beed26904c899fead
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2019-08-09 15:54:48 +0800

    Update run-kadakota.sh

--- k8s-manifest/run-kadakota.sh
+++ k8s-manifest/run-kadakota.sh
@@ -4,4 +4,4 @@ kubectl apply -f secrets.yaml
 kubectl create configmap redis-config --from-file redis.conf
 kubectl apply -f redis-service.yaml
 kubectl apply -f mysql-service.yaml
-kubectl apply -f springboot-app.yaml
+kubectl apply -f springboot-basic-auth.yaml
